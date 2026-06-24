13ececbb38f369ee7f42ad495ed6150de1fba245
venkat <you@example.com>
2026-06-24 12:47:31 +0530

    chore: initialize Spring Boot project with core dependencies

---
+++ src/main/java/com/taskflow/TaskflowProApplication.java
@@ -0,0 +1,13 @@
+package com.taskflow.taskflow_pro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskflowProApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskflowProApplication.class, args);
+	}
+
+}
